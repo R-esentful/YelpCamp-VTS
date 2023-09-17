e6c214bc8c3e07dd6efdcab5b52db4ff88e0db6f
R-esentful <raneljohnt@gmail.com>
2023-09-18 00:27:23 +0800

    Updated main driver

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,7 @@
+import { Request, Response, NextFunction, json, urlencoded } from "express";
 import app, { PORT } from "@utils/express";
+import morgan from "morgan";
+import cors from "cors";
 
 /** ROUTES  **/
 import authRoutes from "@routes/auth.routes";
@@ -11,10 +14,25 @@ import connectDB from "@configs/db.configs";
 
 connectDB();
 
+app.use(morgan("dev"));
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use;
+
 app.use("/auth", authRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/users", userRoutes);
 app.use("/reviews", reviewRoutes);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const { status = 500, message, field } = err;
+
+  if (field) {
+    res.status(status).json({ status: status, field: field, message: message });
+  } else {
+    res.status(status).json({ status: status, message: message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Listening in PORT ${PORT}`);
