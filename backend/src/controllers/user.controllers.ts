2e3523712ebf2e9fa0d132c521dbfffd0071dcde
R-esentful <raneljohnt@gmail.com>
2023-09-18 00:26:08 +0800

    Updated controllers

---
+++ backend/src/controllers/user.controllers.ts
@@ -0,0 +1,34 @@
+/**
+ * This file contains all the business logic for Users.
+ */
+
+import { Request, Response, NextFunction } from "express";
+
+// Utilities
+import { wrapper } from "@utils/general";
+
+// Models
+import User from "@models/user.models";
+
+/**
+ * @desc Creates a new user
+ * @route /users
+ * @method POST
+ */
+
+export const newUser = wrapper(async (req: Request, res: Response, next: NextFunction) => {
+  const { provider } = req.body;
+  switch (provider) {
+    case "EMAIL":
+      const queryUser = await new User({ ...req.body, provider: "EMAIL" }).save();
+      return res.status(201).json({ message: "YelpCamp account successfully created." });
+
+    //TODO:
+    case "FACEBOOK":
+      console.log("Facebook");
+
+    //TODO:
+    case "GOOGLE":
+      console.log("Google");
+  }
+});
