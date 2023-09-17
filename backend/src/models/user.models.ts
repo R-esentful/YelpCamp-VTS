0a91e9b9ec22622eec52c869635e1312b1659a67
R-esentful <raneljohnt@gmail.com>
2023-09-18 00:26:30 +0800

    Updated models

---
+++ backend/src/models/user.models.ts
@@ -0,0 +1,39 @@
+/**
+ * This file is the main schema for user.
+ * TODO:
+ *  - User Interface
+ */
+
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  firstName: { type: String, required: [true, "Please provide a valid first name"] },
+  lastName: { type: String, required: [true, "Please provide a valid last name"] },
+  emailAddress: {
+    type: String,
+    required: [true, "Please provide an email."],
+    unique: [true, "Email already exists"],
+  },
+  password: { type: String, default: "Not Applicable" },
+  provider: {
+    type: String,
+    enum: ["EMAIL", "GOOGLE", "FACEBOOK"],
+    required: [true, "Please input a provider."],
+    validate: {
+      validator: function (provider: string) {
+        return ["EMAIL", "GOOGLE", "FACEBOOK"].includes(provider);
+      },
+      message: "Invalid provider.",
+    },
+  },
+  // TODO
+  // campgrounds: {},
+  // reviews: {},
+  profileImage: { type: String, default: "None" },
+  bio: { type: String, default: "No bio indicated." },
+  type: { type: String, enum: ["User", "Admin"], default: "User" },
+  allowNotification: { type: Boolean, default: true },
+});
+
+const User = model("User", userSchema);
+export default User;
