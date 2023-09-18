b05bd90abc6546648c8a21ad549f5089bf5055ed
R-esentful <raneljohnt@gmail.com>
2023-09-19 00:01:10 +0800

    Updated campground routes

--- backend/src/routes/campground.routes.ts
+++ backend/src/routes/campground.routes.ts
@@ -1,5 +1,20 @@
+/**
+ * This file contains all the API Routes for Users.
+ */
 import { Router } from "express";
 
+// Controllers
+import { getAllCampground, newCampground } from "@controllers/campground.controllers";
+
+// Middlewares & DTO's
+import { _dtoMiddleware } from "@middlewares/dto.middleware";
+import { campgroundValidator } from "@dto/campground.dto";
+
 const campgroundRoutes = Router();
+
+campgroundRoutes
+  .route("/")
+  .get(getAllCampground)
+  .post(campgroundValidator, _dtoMiddleware, newCampground);
 
 export default campgroundRoutes;
