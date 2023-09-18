f025b0f7852dffc9dd86d512aa4d559851d84d5b
R-esentful <raneljohnt@gmail.com>
2023-09-19 00:00:49 +0800

    Updated campground controllers

---
+++ backend/src/controllers/campground.controllers.ts
@@ -0,0 +1,35 @@
+/**
+ * This file contains all the Business Logic for Campgrounds.
+ */
+import { Request, Response, NextFunction } from "express";
+
+// Utilities
+import { wrapper } from "@utils/general";
+
+// Models
+import Campground from "@models/campground.models";
+
+/**
+ * @desc Gets all the campgrounds
+ * @route /campgrounds
+ * @method GET
+ */
+export const getAllCampground = wrapper(async (req: Request, res: Response) => {
+  const campgrounds = await Campground.find({});
+
+  return res.status(200).json({ campgrounds: campgrounds });
+});
+
+/**
+ * @desc Gets all the campgrounds
+ * @route /campgrounds
+ * @method GET
+ */
+export const newCampground = wrapper(async (req: Request, res: Response) => {
+  const campground = await new Campground({ ...req.body }).save();
+
+  return res.status(201).json({
+    message: `Campground ${campground.campName} successfully created.`,
+    campground: campground,
+  });
+});
