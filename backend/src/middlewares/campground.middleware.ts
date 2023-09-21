9034d771ca39122e69cc4a6a602f901b63357e38
R-esentful <raneljohnt@gmail.com>
2023-09-22 05:53:49 +0800

    Updated middlewares

--- backend/src/middlewares/campground.middleware.ts
+++ backend/src/middlewares/campground.middleware.ts
@@ -1,13 +1,24 @@
 /**
  * This file contains all of middlewares for campground.
  */
+
+import mongoose from "mongoose";
 import { Request, Response, NextFunction } from "express";
 import { CustomError, wrapper } from "@utils/general";
+
+// Models
 import Campground from "@models/campground.models";
 
 // TODO: FIX THIS SHIT
 export const checkCampgroundId = wrapper(
   async (req: Request, res: Response, next: NextFunction) => {
+    /**
+     * Check if campground ID is valid
+     */
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return next(new CustomError("Invalid campground ID", 400, "id"));
+    }
+
     const campground = await Campground.findById(req.params.id);
 
     if (!campground) {
