68caa058e3fc46d24342df5dc49113bbf6d33916
R-esentful <raneljohnt@gmail.com>
2023-09-22 21:32:08 +0800

    Updated routes

--- backend/src/routes/reviews.routes.ts
+++ backend/src/routes/reviews.routes.ts
@@ -1,5 +1,13 @@
+import { getAllCampgroundReview, getCampgroundReview } from "@controllers/review.controllers";
+import { checkCampgroundId } from "@middlewares/campground.middleware";
+import { checkReviewId } from "@middlewares/review.middleware";
 import { Router } from "express";
 
 const reviewRoutes = Router();
+
+reviewRoutes.use(checkCampgroundId);
+
+reviewRoutes.route("/").get(getAllCampgroundReview);
+reviewRoutes.route("/:reviewId").get(checkReviewId, getCampgroundReview);
 
 export default reviewRoutes;
