98c509fd6391d257a56a6f4409c880be4dd5ac2d
R-esentful <raneljohnt@gmail.com>
2023-09-22 21:31:38 +0800

    Updated controllers

---
+++ backend/src/controllers/review.controllers.ts
@@ -0,0 +1,62 @@
+/**
+ * This file contains all the Business Logic for Reviews on the campground.
+ */
+import { Request, Response } from "express";
+import { wrapper } from "@utils/general";
+import Review from "@models/review.models";
+import Campground from "@models/campground.models";
+
+/**
+ * @desc Gets all of the reviews of the campground
+ * @route /campgrounds/:id/reviews
+ * @method GET
+ */
+export const getAllCampgroundReview = wrapper(async (req: Request, res: Response) => {
+  const reviews = await Review.find({});
+
+  return res.status(200).json({ reviews: reviews });
+});
+
+/**
+ * @desc Gets all of the reviews of the campground
+ * @route /campgrounds/:id/reviews
+ * @method GET
+ */
+export const getCampgroundReview = wrapper(async (req: Request, res: Response) => {
+  const review = await Review.findById(req.body.reviewId);
+
+  return res.status(200).json({
+    review: review,
+  });
+});
+
+/**
+ * @desc Creates a review of the specific campground
+ * @route /campgrounds/:id/reviews/:reviewId
+ * @method POST
+ */
+export const newCampgroundReview = wrapper(async (req: Request, res: Response) => {
+  const campground = await Campground.findById(req.body.id);
+
+  const reviews = await new Review({ ...req.body }).save();
+
+  return res.status(200).json({
+    message: `Review for ${campground?.campName} successfully created.`,
+    review: reviews,
+  });
+});
+
+/**
+ * @desc Creates a review of the specific campground
+ * @route /campgrounds/:id/reviews/:reviewId
+ * @method PATCH
+ */
+export const editCampgroundReview = wrapper(async (req: Request, res: Response) => {
+  const review = await Review.findByIdAndUpdate(req.params.reviewId, {
+    $set: { ...req.body },
+  }).populate("campground");
+
+  return res.status(200).json({
+    review: review,
+  });
+});
