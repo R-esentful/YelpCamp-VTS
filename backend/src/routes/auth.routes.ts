0e0838631a4679158691263682d888f3a9a2ff97
R-esentful <raneljohnt@gmail.com>
2023-09-24 16:33:53 +0800

    Updated routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+// Middlewares
+import { jwtValidator } from "@middlewares/authentication.middleware";
+
+// Controllers
+import { authJWT } from "@controllers/auth.controllers";
+
 const authRoutes = Router();
+
+authRoutes.route("/jwt").post(jwtValidator, authJWT);
 
 export default authRoutes;
