bdc572d2e675b3b584ec073c3c1670ffd2a65e7b
R-esentful <raneljohnt@gmail.com>
2023-10-11 22:51:38 +0800

    Added api

---
+++ frontend/src/api/signup.ts
@@ -0,0 +1,24 @@
+import { FormikHelpers } from "formik";
+import { FormikSignUp } from "@interfaces/FormikInterface";
+import YelpCamp from "api/config";
+
+export const initialSignupVal: FormikSignUp = {
+  emailAddress: "",
+  password: "",
+  confirmPassword: "",
+  loading: false,
+};
+
+export const handleSignup = async (
+  state: FormikSignUp,
+  action: FormikHelpers<typeof initialSignupVal>
+) => {
+  try {
+    action.setFieldValue("loading", true);
+    const { loading, ...rest } = state;
+    const response = await YelpCamp.post("/users", { ...rest, provider: "EMAIL" });
+    action.setFieldValue("loading", false);
+  } catch (e) {
+    console.log(e);
+  }
+};
