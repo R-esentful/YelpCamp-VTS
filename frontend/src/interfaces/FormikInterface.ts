476174929b1ba322861ede55e121fcd61b080979
R-esentful <raneljohnt@gmail.com>
2023-10-15 11:49:42 +0800

    Updated interfaces

--- frontend/src/interfaces/FormikInterface.ts
+++ frontend/src/interfaces/FormikInterface.ts
@@ -2,5 +2,12 @@ export interface FormikSignUp {
   emailAddress: string;
   password: string;
   confirmPassword: string;
+  provider: string;
+}
+
+export interface FormikSignin {
+  emailAddress: string;
+  password: string;
   loading: boolean;
+  provider: string;
 }
