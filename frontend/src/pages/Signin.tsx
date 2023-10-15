d8044fb71f3915b8ec1f5f01456962f07f523471
R-esentful <raneljohnt@gmail.com>
2023-10-15 11:50:05 +0800

    Updated pages

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,11 +1,21 @@
-import { BiLogoFacebookCircle } from "react-icons/bi";
-import { FcGoogle } from "react-icons/fc";
-import { Link } from "react-router-dom";
-import ThemeButton from "@components/theme";
+import { Formik, Form, Field } from "formik";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ThemeButton from "@components/ui/theme";
 import sign from "assets/sign.jpg";
 import logo from "@assets/logo.png";
+import FacebookComponent from "@components/facebook";
+import GoogleComponent from "@components/google";
+import EyeIcon from "@components/icons/eye";
+import EyeSlashIcon from "@components/icons/eye-slash";
+import { handleSignin, initialSigninVal } from "@actions/signin";
+import { signinSchema } from "@schema/schema";
 
 function Signin() {
+  const navigate = useNavigate();
+  const [viewPass, setViewPass] = useState<boolean>(false);
+  const [showPass, setShowPass] = useState<boolean>(false);
+
   return (
     <div className="h-screen grid grid-cols-1 lg:grid-cols-2 ">
       <section className=" hidden lg:block">
@@ -30,43 +40,85 @@ function Signin() {
                   Discover the Great Outdoors with YelpCamp!
                 </p>
               </div>
-              <form action="" className="mt-4 w-full ">
-                {/* Email Address */}
-                <div className="form-control w-full  ">
-                  <label className="label">
-                    <span className="label-text">Email Address</span>
-                  </label>
-                  <input type="text" placeholder="Type here" className="input bg-slate-200 " />
-                </div>
-                {/* Password */}
+              <Formik
+                initialValues={initialSigninVal}
+                validationSchema={signinSchema}
+                onSubmit={(state, action) => handleSignin(state, action, navigate)}
+              >
+                {({ errors, touched, values }) => (
+                  <Form className="mt-4 w-full">
+                    {/* Email Address */}
+                    <div className="form-control w-full  ">
+                      <label className="label">
+                        <span className="label-text text-secondary">Email Address</span>
+                        {errors.emailAddress && touched.emailAddress && (
+                          <span className="label-text-alt text-error">{errors.emailAddress}</span>
+                        )}
+                      </label>
+                      <Field
+                        type="text"
+                        name="emailAddress"
+                        placeholder="Email address"
+                        className="input bg-slate-200 "
+                      />
+                    </div>
+                    {/* Password */}
 
-                <div className="form-control w-full   mt-3">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input type="text" placeholder="Type here" className="input bg-slate-200 " />
-                </div>
+                    <div className="form-control w-full   mt-3">
+                      <label className="label">
+                        <span className="label-text text-secondary">Password</span>
+                        {errors.password && touched.password && (
+                          <span className="label-text-alt text-error">{errors.password}</span>
+                        )}
+                      </label>
+                      <div className="join w-full">
+                        <Field
+                          name="password"
+                          type={showPass ? "text" : "password"}
+                          placeholder="Password"
+                          className="input bg-slate-200 join-item w-full"
+                          onClick={() => setViewPass(true)}
+                        />
+                        {viewPass && (
+                          <button
+                            className="btn btn-ghost rounded-tl-none rounded-bl-none  bg-slate-200"
+                            type="button"
+                            onClick={() => setShowPass(!showPass)}
+                          >
+                            {showPass ? (
+                              <EyeSlashIcon width="24" height="24" className="join-item" />
+                            ) : (
+                              <EyeIcon width="24" height="24" className="join-item" />
+                            )}
+                          </button>
+                        )}
+                      </div>
+                    </div>
 
-                <button type="submit" className="btn btn-primary w-full  mt-8 ">
-                  LOGIN
-                </button>
-              </form>
+                    <button
+                      type="submit"
+                      className={`btn  w-full mt-8 ${
+                        values.loading === true
+                          ? "pointer-events-none bg-[#D3D3D3] border-none"
+                          : "btn-primary"
+                      }`}
+                    >
+                      {values.loading === true ? (
+                        <span className="loading loading-spinner text-primary"></span>
+                      ) : (
+                        "CREATE AN ACCOUNT"
+                      )}
+                    </button>
+                  </Form>
+                )}
+              </Formik>
             </div>
 
             <div>
               <div className="divider my-8 w-full  text-sm">OR</div>
               <div className="w-full  grid grid-cols-1 gap-4  lg:grid-cols-2">
-                <button className="btn btn-ghost">
-                  <BiLogoFacebookCircle
-                    color="blue"
-                    className="align-middle my-auto  w-[24px] h-[24px]"
-                  />{" "}
-                  <span>Facebook</span>
-                </button>
-                <button className="btn btn-ghost">
-                  <FcGoogle className="align-middle my-auto  w-[23px] h-[23px]" />{" "}
-                  <span>google</span>
-                </button>
+                <FacebookComponent />
+                <GoogleComponent />
               </div>
               <p className="text-sm text-center mt-4">
                 Don't have an account?{" "}
