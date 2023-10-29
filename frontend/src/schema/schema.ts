8763d9ea0e9e50e9c755c20e08bd4ddb5faa1985
R-esentful <raneljohnt@gmail.com>
2023-10-29 16:43:19 +0800

    Updated schema

--- frontend/src/schema/schema.ts
+++ frontend/src/schema/schema.ts
@@ -13,3 +13,17 @@ export const signinSchema = yup.object().shape({
   emailAddress: yup.string().email("Must be a valid emailaddress.").required("Required *"),
   password: yup.string().required("Required *").min(8, "Passowrd is too short."),
 });
+
+export const newCampgroundSchema = yup.object().shape({
+  campName: yup.string().required("Required *"),
+  type: yup
+    .string()
+    .oneOf(["PUBLIC", "PRIVATE"], "Camp Type should be PUBLIC or PRIVATE.")
+    .required("Required *"),
+  location: yup.string().required("Required *"),
+  description: yup
+    .string()
+    .required("Required *")
+    .max(250, "Maximum of 250 characters only")
+    .min(20, "Minimum of 20 characters"),
+});
